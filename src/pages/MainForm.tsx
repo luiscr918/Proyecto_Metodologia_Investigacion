23db92798ef696e1842e4b810f4e83ef28a4dbc7
luiscr918 <lc9107040@gmail.com>
2025-02-03 19:46:01 -0500

    diseño de formaulario realizado

--- src/pages/MainForm.tsx
+++ src/pages/MainForm.tsx
@@ -1,13 +1,38 @@
-import { Link } from "react-router-dom"
+
 
 
 export const MainForm = () => {
     return (
-        <>
-            <h1>este será el main y desde aqui se mandará bien sea a la seccion de alumnos o profesores</h1>
-            <Link to='/profesores' className="text-blue-500">ir a profesores</Link>
-            <br />
-            <Link to='/estudiantes' className="text-blue-500" >ir a estudiantes</Link>
-        </>
+        <div className="fondoMaster">
+            <section>
+                <div className="flex flex-col items-center justify-center px-6 py-8  mx-auto md:h-screen lg:py-0">
+
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Sign in to your account
+                            </h1>
+                            <form className="space-y-4 md:space-y-6" action="#">
+                                <div>
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                    <input type="email" name="email" id="email" className="transition-all duration-300 hover:bg-blue-100  bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="transition-all duration-300 hover:bg-blue-100  bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                </div>
+                                {/* //botoncito de mostrar contraseña */}
+                                <div className="justify-self-end">
+                                    <button type="button" className="w-auto text-white bg-emerald-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-colors duration-500 dark:bg-cyan-300 dark:hover:bg-cyan-200 dark:focus:ring-primary-800">Show Password</button>
+                                </div>
+                                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-colors duration-500 dark:bg-blue-700 dark:hover:bg-blue-400 dark:focus:ring-primary-800">Sign in</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+        </div>
     )
 }

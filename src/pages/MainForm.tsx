a6aa8a0826cc748dc7b8614cb4512e32b87a0374
luiscr918 <lc9107040@gmail.com>
2025-02-17 10:12:46 -0500

    añadido beta andres

--- src/pages/MainForm.tsx
+++ src/pages/MainForm.tsx
@@ -1,10 +1,26 @@
 import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
 import { useState } from "react";
-
+import { useNavigate } from 'react-router-dom';
+interface Login{
+    email:string;
+    password:string;
+}
 
 export const MainForm = () => {
     //hook useState para mostrar u ocultar la constrasenia
     const [showPassword, setShowpassword] = useState<boolean>(false);
+    //usando useState para manejar inputs
+    const [form, setForm] = useState<Login>({
+        email:'',
+        password:''
+    });
+
+    //funciones momentanea para navegar entre paginas
+    const navigate = useNavigate();
+    const irProfesores = () => {
+
+        navigate('/profesores');
+    }
 
     return (
         <div className="fondoMaster">
@@ -19,16 +35,21 @@ export const MainForm = () => {
                             <form className="space-y-4 md:space-y-6 relative" action="#">
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" className="transition-all duration-300 hover:bg-blue-100  bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                                    <input type="email" name="email" id="email" 
+                                    
+                                    className="transition-all duration-300 hover:bg-blue-100  bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type={showPassword ? 'password' : 'text'} name="password" id="password" placeholder="••••••••" className="transition-all duration-300 hover:bg-blue-100  bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    <input type={showPassword ? 'password' : 'text'} 
+                                    
+                                    name="password" id="password" placeholder="••••••••" 
+                                    className="transition-all duration-300 hover:bg-blue-100  bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                                     {/* //botoncito de mostrar contraseña */}
 
                                     <button
                                         type="button"
-                                        className=" absolute right-3 bottom-1/3 transform translate-y-1/4  text-gray-500"
+                                        className=" absolute right-5 bottom-1/3 transform translate-y-1/4  text-gray-500"
                                         onClick={() => setShowpassword(!showPassword)}
                                     >
                                         {showPassword ? (
@@ -41,7 +62,11 @@ export const MainForm = () => {
 
                                 </div>
 
-                                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-colors duration-500 dark:bg-blue-700 dark:hover:bg-blue-400 dark:focus:ring-primary-800">Sign in</button>
+                                <button type="submit"
+                                    onClick={irProfesores}
+                                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300
+                                  font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-colors duration-500 dark:bg-blue-700
+                                 dark:hover:bg-blue-400 dark:focus:ring-primary-800">Sign in</button>
                             </form>
                         </div>
                     </div>
--- src/pages/Profesores.tsx
+++ src/pages/Profesores.tsx
@@ -2,6 +2,48 @@
 
 export const Profesores = () => {
   return (
-    <div>Profesores</div>
+    <>
+      <div className="contenedor-principal">
+        <div className="contenedor-info">
+          <p >PERIODO ACADEMICO</p>
+          <p>TUTOR PROF.</p>
+
+        </div>
+        <div className="contenedor-logo">
+          <img className="logo-img" src="/src/assets/imgs/logoPageProf.png" alt="Logo ITSQMET" />
+        </div>
+      </div>
+      {'\n'}
+      {'\n'}
+      <div className="container">
+        <div className="registro-container mx-auto">
+          <form className="mx-auto" action="/validar" method="post">
+            <h4 className="text-center textF">Registro de Prácticas Vinculación</h4>
+            <div className="mb-3 mt-3">
+              <label htmlFor="nombre" className="form-label tituloF">Hora inicio:</label>
+              <input type="number" className="form-control" name="hI" id="horaI" />
+            </div>
+            <div className="mb-3 mt-3">
+              <label className="form-label tituloF">Hora Final: </label>
+              <input type="number" className="form-control" name="hF" id="HoraF" />
+            </div>
+
+            <div className="mb-3 mt-3">
+              <label className="form-label tituloF">Entidad Beneficiaria: </label>
+              <input type="text" className="form-control" name="eB" id="entidadB" />
+            </div>
+
+            <div className="mb-3 mt-3">
+              <label className="form-label tituloF">Hora Final Visita: </label>
+              <input type="number" className="form-control" name="hFv" id="horaFV" />
+            </div>
+            {'\n'}
+            <input type="submit" className="btn-primary" value="Registrar" />
+            {'\n'}
+
+          </form>
+        </div>
+      </div>
+    </>
   )
 }

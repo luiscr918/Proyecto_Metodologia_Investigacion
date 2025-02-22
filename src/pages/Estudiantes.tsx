0701b5881a2d135154bd21e492f8f6c698cc9b0d
luiscr918 <lc9107040@gmail.com>
2025-02-21 21:08:46 -0500

    pagina de estudiantes lista solo falta agregar nombre de tutor asignado pero solo si deseo

--- src/pages/Estudiantes.tsx
+++ src/pages/Estudiantes.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 //no borrar esto, es lo que me permite obtener datos del profesor desde  la DB
 interface Estudiante {
   ci_estudiante: string;
@@ -6,11 +8,12 @@ interface Estudiante {
   contrasenia: string;
   primer_nombre: string
   primer_apellido: string;
-  horas_totales:string;
+  horas_totales: string;
+  entidad_beneficiaria: string;
 }
 export const Estudiantes = () => {
 
-//DESDE AQUI TAMPOCO BORRAR
+  //DESDE AQUI TAMPOCO BORRAR
   const [estudiante, setEstudiante] = useState<Estudiante | null>(null);
   // Método para obtener el estudiante que ha iniciado sesión
   const fetchEstudiante = async () => {
@@ -36,24 +39,56 @@ export const Estudiantes = () => {
   useEffect(() => {
     fetchEstudiante();
   }, []);
-//HASTA AQUI A PARTIR DE AQUI SI HACER LO QUE SEA
-
+  //HASTA AQUI A PARTIR DE AQUI SI HACER LO QUE SEA
+  //use navigate para regresar al main
+  const navigate = useNavigate();
+  const returnHome = () => {
+    navigate('/')
+  }
   return (
-    <div className="container m-5">
-      <h1>Datos del Estudiante</h1>
-      {estudiante
-        ?
-        (
-          <div className="card">
-            <h3>{estudiante.ci_estudiante}</h3>
-            <h3>{estudiante.primer_nombre} {estudiante.primer_apellido}</h3>
-            <p>Email: {estudiante.email}</p>
+    <>
+      <header className="bg-gray-900 text-white py-4 px-6 shadow-lg flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Estudiantes</h1>
+        <Link to={'/'}>
+          <img src="/src/assets/imgs/logoStudents.png" alt="Logo" className="h-15 w-45 " />
+        </Link>
+      </header>
+
+      <div className="bg-gray-100 p-6 rounded-lg shadow-lg max-w-lg mx-auto mt-10">
+        <form className="space-y-4">
+          <div className="flex items-center">
+            <label className="text-lg font-semibold text-gray-700 w-32">Nombre:</label>
+            <input type="text" readOnly
+              value={estudiante?.primer_nombre.concat(' ', estudiante.primer_apellido)}
+              className="flex-1 p-3 border-2 border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-900" />
           </div>
-        )
-        :
-        (
-          <p>Cargando información del estudiante...</p>
-        )}
-    </div>
+
+          <div className="flex items-center">
+            <label className="text-lg font-semibold text-gray-700 w-32">Carrera:</label>
+            <input type="text" readOnly
+              className="flex-1 p-3 border-2 border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-900" />
+          </div>
+
+          <div className="flex items-center">
+            <label className="text-lg font-semibold text-gray-700 w-32">Entidad Beneficiaria:</label>
+            <input type="text" readOnly
+              value={estudiante?.entidad_beneficiaria}
+              className="flex-1 p-3 border-2 border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-900" />
+          </div>
+
+          <div className="flex items-center">
+            <label className="text-lg font-semibold text-gray-700 w-32">Horas Totales Completadas:</label>
+            <input type="text" readOnly
+              className="flex-1 p-3 border-2 border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-900" />
+          </div>
+
+          <button type="button"
+            onClick={returnHome}
+            className="w-full bg-gray-900 text-white py-3 rounded-lg font-bold hover:bg-blue-ring-blue-900 transition">
+            Back to Home
+          </button>
+        </form>
+      </div>
+    </>
   );
 };

04d89ca48f2905127fc3b347a3ff848affd5d7be
luiscr918 <lc9107040@gmail.com>
2025-02-24 20:49:33 -0500

    form admin listo para trabajarle

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,7 +1,121 @@
+import { FooterComponent } from "../components/FooterComponent"
+import { HeaderComponent } from "../components/HeaderComponent"
 
 
 export const Admin = () => {
-  return (
-    <div>Admin</div>
-  )
+    return (
+        <>
+            <HeaderComponent title="welcome Admin" />
+            <h1 className="text-2xl font-bold mt-1 mb-6 text-center">Registration Page</h1>
+            <div className="container mx-auto py-8 ">
+                {/* form para añadir nuevo estudiante */}
+                <h1 className="text-2xl font-bold  mb-6 text-center">Registration Form to Student</h1>
+                <form className="w-full max-w-sm mx-auto bg-white p-8 rounded-md shadow-md">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="ciE">Cedula</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="ciE" name="cE" placeholder="John Doe" />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nameE">Nombre</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="nameE" name="nE" placeholder="John Doe" />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellidoE">Apellido</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="apellidoE" name="aE" placeholder="John Doe" />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="entidadB">Entidad Beneficiaria</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="entidadB" name="eB" placeholder="John Doe" />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="carrera">Carrera</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="carrera" name="carr" placeholder="John Doe" />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="rolE">Rol</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="rolE" name="rE" placeholder="John Doe" />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="pAc">Periodo Académico</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="pAc" name="pA" placeholder="John Doe" />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="emailE">Email</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="email" id="emailE" name="emailEs" placeholder="john@example.com" />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="passwordE">Password</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="password" id="passwordE" name="passwordEs" placeholder="********" />
+                    </div>
+
+                    <button
+                        className="w-full bg-indigo-500 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300"
+                        type="submit">Register</button>
+                </form>
+                <br />
+                <h1 className="text-2xl font-bold  mb-6 text-center">Registration Form to Teacher</h1>
+                {/* //form para añadir nuevo profesor */}
+                <form className="w-full max-w-sm mx-auto bg-white p-8 rounded-md shadow-md m-11">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="ciP">Cedula</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="ciP" name="cP" placeholder="John Doe" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="esAsig">Estudiante Asignado</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="esAsig" name="eA" placeholder="John Doe" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nameP">Nombre</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="nameP" name="nP" placeholder="John Doe" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apP">Apellido</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="apP" name="apellP" placeholder="John Doe" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="rolP">Rol</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="text" id="rolP" name="rP" placeholder="John Doe" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="emailP">Email</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="email" id="emailP" name="eP" placeholder="john@example.com" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="passwordP">Password</label>
+                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                            type="password" id="passwordP" name="pasP" placeholder="********" />
+                    </div>
+
+
+                    <button
+                        className="w-full bg-indigo-500 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300"
+                        type="submit">Register</button>
+                </form>
+
+
+            </div>
+            <FooterComponent />
+        </>
+    )
 }
